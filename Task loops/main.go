9c6ec65d8c755f50a4b4e91eeec4693b683eac76
Amr Maher Fawzy <MaherAmr871@Gmail.com>
2026-04-27 02:48:45 +0300

    add Task loops/main.go

---
+++ Task loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func countAlpha(s string) int {
+	count := 0
+
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z'{
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(countAlpha("Hello world"))
+}
